4212683847aca31003e99e6dcc68ff55e3bebcb9
arjunbnair97 <arjunbnair97@gmail.com>
2020-05-09 05:29:24 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<!--
+Template Name: Ossibird
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>Ossibird</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<style type="text/css">
+#video {
+	font-size: 9px;
+	line-height: normal;
+	height: 272px;
+	width: 480px;
+}
+</style>
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="topspacer bgded overlay" style="background-image:url('images/demo/backgrounds/04.jpg');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_left">
+        <ul class="nospace">
+          <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+          <li><i class="fa fa-envelope-o"></i> info@domainBASKET.com</li>
+        </ul>
+      </div>
+      <div class="fl_right">
+        <ul class="nospace">
+          <li><a href="index.html"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="#">Register</a></li>
+        </ul>
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html">DOMAIN BASKET</a></h1>
+      </div>
+      <!-- ################################################################################################ -->
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="pages/adminbase.php">Home</a></li>
+          <li><a class="drop" href="#">Pages</a>
+            <ul>
+              <li><a href="pages/gallery.html">Gallery</a></li>
+              <li><a href="pages/full-width.html">Full Width</a></li>
+              <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+              <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+              <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Domains</a>
+            <ul>
+              <li><a href="#">Level 2</a></li>
+              <li><a class="drop" href="#">Level 2 + Drop</a>
+                <ul>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Level 2</a></li>
+            </ul>
+          </li>
+          <li>SERVERS</li>
+          <li><a href="#">Link Text</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear">
+    <article>
+      <h2 class="heading">Simple and easy.</h2>
+      <p>Get the latest and unique domain names for your own websites here at Domain Basket.</p>
+      <footer>
+        <ul class="nospace inline pushright">
+          <li><a class="btn" href="#">Sign in</a></li>
+          <li><a class="btn inverse" href="#">Login</a></li>
+        </ul>
+      </footer>
+    </article>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  </div>
+  
+  
+  
+  
+  
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <article class="one_quarter first">
+      <h6 class="heading font-x3">HOST YOUR WEBSITE!</h6>
+      <p class="btmspace-30">You can buy any one of the 3 offers that we provide in our site. </a></p>
+      <footer><a class="btn" href="#">Read More &raquo;</a></footer>
+    </article>
+    
+    
+    <article class="one_quarter">
+      <h6 class="heading"><a href="#">PREMIUM</a></h6>
+      <a href="#"><img class="btmspace-15" src="images/demo/premium.jpg" alt=""></a>
+      <p class="btmspace-30">
+      Starts at<br>
+<font size="6"> Rs.59/mo</font><br>
+Host One Website<br>
+5MB Space<br>
+1 Email IDs<br>
+       <a href="#"></a></p>
+      <br>
+      <br>
+      <br>
+      <br>
+        <li><a class="btn" href="#">BUY</a></li>
+      </article>
+      
+      
+    <article class="one_quarter">
+      <h6 class="heading"><a href="#">ADVANCED</a></h6>
+      <a href="#"><img class="btmspace-15" src="images/demo/advanced.png" alt=""></a>
+      <p class="btmspace-30">
+      Starts at<br>
+<font size="6">Rs.179/mo</font><br>
+Host THREE Website<br>
+15MB Space<br>
+3 email ID <br>
+     <a href="#"></a></p>
+      <br>
+      <br>
+      <br>
+       <li><a class="btn" href="#">BUY</a></li>
+    </article>
+    
+    
+    <article class="one_quarter">
+      <h6 class="heading"><a href="#">ELITE</a></h6>
+ 
+      <a href="#"><img class="btmspace-15" src="images/demo/elite.gif" alt=""></a>
+      
+      <p class="btmspace-30"> 
+      Starts at<br>
+<font size="6" >Rs.319/mo</font><br>
+Host FIVE Websites<br>
+25MB Space<br>
+5 Email IDs<a href="#"></a></p>
+      <br>
+      <br>
+      <br>
+       <li><a class="btn" href="#">BUY</a></li>
+    </article>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+
+<div class="topspacer bgded overlay" style="background-image:url('images/demo/backgrounds/01.jpg');"> 
+
+  <div class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="group">
+      <section class="one_half first"> 
+        <!-- ################################################################################################ -->
+        <h6 class="heading">DOMAIN NAMES</h6>
+        <p class="btmspace-30">From only ₨810.14/yr!*
+Get your domain name today! Search now..</p>
+<br>
+<br>
+
+<footer>
+              <form class="group" method="post" action="#">
+                <fieldset>
+                  <legend>Sign-Up:</legend>
+                  <input type="" value="" placeholder="Domain name">
+                  <button class="fa fa-sign-in"   type="submit" title="Submit"><em>Search</em></button>
+                </fieldset>
+              </form>
+            </footer>
+<br>
+<br>
+        <ul class="nospace group">
+          <li class="one_half first">
+            <article><a href="#"><i class="icon btmspace-30 fa fa-language"></i></a>
+              <h6 class="heading font-x1">Tortor rhoncus</h6>
+              <p>Odio dignissim eros pretium bibendum ligula ultricies eros ut tincidunt nunc tortor sit <a href="#">[&hellip;]</a></p>
+            </article>
+          </li>
+          <li class="one_half">
+            <article><a href="#"><i class="icon btmspace-30 fa fa-leaf"></i></a>
+              <h6 class="heading font-x1">Nibh aliquam non</h6>
+              <p>Amet felis maecenas dapibus tellus diam sollicitudin ac congue leo molestie non nullam <a href="#">[&hellip;]</a></p>
+            </article>
+          </li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </section>
+      <section class="one_half"> 
+        <!-- ################################################################################################ -->
+        
+       
+        <!-- ################################################################################################ -->
+        <div class="active" id="video">
+        <video src="images/demo/VID1.mp4" width="480" height="272" controls></video>
+        
+        </div>
+   
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/07.jpg');">
+  <article class="hoc container clear cta"> 
+    <!-- ################################################################################################ -->
+    <div class="three_quarter first">
+      <h6 class="nospace">At posuere neque nam magna</h6>
+      <p class="nospace">Dui interdum eu purus in cursus egestas mauris in sed rutrum turpis integer id</p>
+    </div>
+    <footer class="one_quarter"><a class="btn medium" href="#">Fringilla malesuada &raquo;</a></footer>
+    <!-- ################################################################################################ -->
+  </article>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3"></div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4 bgded overlay" style="background-image:url('images/demo/backgrounds/03.png');">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_third first">
+      <h6 class="heading">DOMAIN BASKET</h6>
+      <nav>
+        <ul class="nospace">
+          <li><a href="index.html"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Cookies</a></li>
+          <li><a href="#">Disclaimer</a></li>
+        </ul>
+      </nav>
+      <p>Join us on our social media networks.</p>
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+        <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+      </ul>
+    </div>
+    <div class="one_third">
+      <h6 class="heading"><b>Contact details</b></h6>
+      <ul class="nospace linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+          24th Street,California,United States Of America
+          </address>
+        </li>
+        <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-envelope-o"></i> info@domainbasket.com</li>
+      </ul>
+    </div>
+    <div class="one_third">
+      <h6 class="heading">Account</h6>
+      <ul class="nospace linklist">
+        <li>
+          
+           <nav>
+        <ul class="nospace">
+          <li><a href="index.html"></a></li>
+          <li><a href="#">My About</a></li>
+          <li><a href="#">My Renewals</a></li>
+          <li><a href="#">Create Account</a></li>
+        </ul>
+      </nav>
+          
+        </li>
+        <li>
+         
+          </article>
+        </li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">Domain basket</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
---
+++ pages/comments.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div class="content">
+  <!-- ################################################################################################ -->
+  <h1>&nbsp;</h1>
+  <div id="comments">
+    <h2>Comments</h2>
+    <ul>
+      <li>Write A Comment</li>
+    </ul>
+    <form action="#" method="post">
+      <div class="one_third first">
+        <label for="name">Name <span>*</span></label>
+        <input type="text" name="name" id="name" value="" size="22" required="required" />
+      </div>
+      <div class="one_third">
+        <label for="email">Mail <span>*</span></label>
+        <input type="email" name="email" id="email" value="" size="22" required="required" />
+      </div>
+      <div class="one_third">
+        <label for="url">Website</label>
+        <input type="url" name="url" id="url" value="" size="22" />
+      </div>
+      <div class="block clear">
+        <label for="comment">Your Comment</label>
+        <textarea name="comment" id="comment" cols="25" rows="10"></textarea>
+      </div>
+      <div>
+        <input type="submit" name="submit" value="Submit Form" />
+        &nbsp;
+        <input type="reset" name="reset" value="Reset Form" />
+      </div>
+    </form>
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+</body>
+</html>
